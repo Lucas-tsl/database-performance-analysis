af5730627f6e2cd1f3a52179087b1780fdfc0f46
Lucas Troteseil <troteseil.lucas@gmail.com>
2026-01-19 12:02:56 +0100

    fix: add and restruct folder

---
+++ sql/script-table1.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS access_logs;
+DROP TABLE IF EXISTS enrollments;
+DROP TABLE IF EXISTS courses;
+DROP TABLE IF EXISTS students;
+
+
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    signup_date TIMESTAMPTZ DEFAULT NOW() 
+);
+
+
+CREATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    category TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+
+CREATE TABLE enrollments (
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id INT NOT NULL REFERENCES students(student_id),
+    course_id INT NOT NULL REFERENCES courses(course_id),
+    enrollment_date TIMESTAMPTZ DEFAULT NOW(),
+    grade INT CHECK (grade BETWEEN 0 AND 100)
+    CONSTRAINT unique_enrollment UNIQUE (student_id, course_id)
+);
+
+
+CREATE TABLE access_logs (
+    log_id BIGSERIAL PRIMARY KEY,
+    student_id INT REFERENCES students(student_id),
+    url_accessed TEXT NOT NULL,
+    access_time TIMESTAMPTZ DEFAULT NOW(),
+    ip_address INE
+);
